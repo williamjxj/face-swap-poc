04485047bf8eed91c5e6661e386357b7c4316451
jxjwilliam <jxjwilliam@sina.com>
2025-05-16 21:56:47 +0800

    move unused files to /tmp to cleanup codes.

--- src/app/payment/layout.js
+++
@@ -1,7 +0,0 @@
-export default function PaymentLayout({ children }) {
-  return (
-    <div className="payment-layout container mx-auto py-8">
-      {children}
-    </div>
-  );
-}
--- src/lib/crypto.js
+++
@@ -1,5 +0,0 @@
-import { CoinbaseCommerce } from 'coinbase-commerce-node'
-
-const client = CoinbaseCommerce.Client.init(process.env.COINBASE_COMMERCE_API_KEY)
-
-export default client;
